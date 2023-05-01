49fce051dd4b103097878d28d69ff3b76264f330
Sarthak Vaish <sarthak@simplifii.com>
2023-05-01 15:46:42 +0530

    Created NFT card

--- pages/store.js
+++ pages/store.js
@@ -6,117 +6,139 @@ import { ethers } from "ethers";
 import axios from "axios";
 
 export default function Store() {
+  const [nfts, setNfts] = useState([]);
+  // const [nfts, setNfts] = useState([{
+  //     price: "1",
+  //     name: "Twelve",
+  //     tokenId: "9",
+  //     supply: "15",
+  //     cover: "https://eventifyv1.vercel.app/download.gif",
+  // }, {
+  //     price: "4",
+  //     name: "Owl Boats",
+  //     tokenId: "41",
+  //     supply: "10",
+  //     cover: "https://ipfs.io/ipfs/bafybeiadpzjd56aie2l6yqiyirwsxnjicoenkqinqpamrpsv3klx2rtk5y/download.png",
+  // }])
 
-    const [nfts, setNfts] = useState([])
-    // const [nfts, setNfts] = useState([{
-    //     price: "1",
-    //     name: "Twelve",
-    //     tokenId: "9",
-    //     supply: "15",
-    //     cover: "https://eventifyv1.vercel.app/download.gif",
-    // }, {
-    //     price: "4",
-    //     name: "Owl Boats",
-    //     tokenId: "41",
-    //     supply: "10",
-    //     cover: "https://ipfs.io/ipfs/bafybeiadpzjd56aie2l6yqiyirwsxnjicoenkqinqpamrpsv3klx2rtk5y/download.png",
-    // }])
+  const [loaded, setLoaded] = useState();
 
-    const [loaded, setLoaded]= useState();
+  useEffect(() => {
+    fetchFvmNfts();
+  }, []);
 
-    useEffect(() => {
-        fetchFvmNfts()
-    }, [])
+  async function fetchFvmNfts() {
+    const modal = new web3modal({
+      network: "mumbai",
+      cacheProvider: true,
+    });
+    const connection = await modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(fvmAddress, gatifyAbi, signer);
+    const data = await contract.fetchNfts();
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await contract.uri(i.tokenId.toString());
+        const meta = await axios.get(tokenUri);
+        console.log(meta);
+        let price = ethers.utils.formatEther(i.price);
+        let item = {
+          price,
+          name: meta.data.name,
+          tokenId: i.tokenId.toNumber(),
+          supply: i.supply.toNumber(),
+          remaining: i.remaining.toNumber(),
+          image: meta.data.image,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoaded(true);
+  }
 
-    async function fetchFvmNfts() {
-        const modal = new web3modal({
-            network: "mumbai",
-            cacheProvider: true,
-        });
-        const connection = await modal.connect();
-        const provider = new ethers.providers.Web3Provider(connection);
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(
-            fvmAddress,
-            gatifyAbi,
-            signer
-        );
-        const data = await contract.fetchNfts();
-        const items = await Promise.all(
-            data.map(async (i) => {
-                const tokenUri = await contract.uri(i.tokenId.toString());
-                const meta = await axios.get(tokenUri);
-                console.log(meta)
-                let price = ethers.utils.formatEther(i.price);
-                let item = {
-                    price,
-                    name: meta.data.name,
-                    tokenId: i.tokenId.toNumber(),
-                    supply: i.supply.toNumber(),
-                    remaining: i.remaining.toNumber(),
-                    image: meta.data.image,
-                };
-                return item;
-            })
-        );
-        setNfts(items);
-        setLoaded(true);
-    }
+  async function buy(item) {
+    const modal = new web3modal({
+      network: "mumbai",
+      cacheProvider: true,
+    });
+    const connection = await modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(fvmAddress, gatifyAbi, signer);
+    const price = ethers.utils.parseUnits(item.price.toString(), "ether");
+    const transaction = await contract.buyNft(item.tokenId, {
+      value: price,
+      gasLimit: 1000000,
+    });
+    await transaction.wait();
+    console.log(transaction);
+    fetchFvmNfts();
+  }
 
-    async function buy(item) {
-        const modal = new web3modal({
-            network: "mumbai",
-            cacheProvider: true,
-        });
-        const connection = await modal.connect();
-        const provider = new ethers.providers.Web3Provider(connection);
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(
-            fvmAddress,
-            gatifyAbi,
-            signer
-        );
-        const price = ethers.utils.parseUnits(item.price.toString(), "ether");
-        const transaction = await contract.buyNft(item.tokenId, {
-            value: price,
-            gasLimit: 1000000,
-        });
-        await transaction.wait();
-        console.log(transaction)
-        fetchFvmNfts();
-    }
-
-    function NftCard(prop) {
-        return(
-            <div>
-                <p>Card</p>
-                <img src={prop.image} alt="" width="200px"/>
-                <p>{prop.price}</p>
-                <p>{prop.name}</p>
-                <p>{prop.tokenId}</p>
-                <p>{prop.supply}</p>
-                <p>{prop.remaining}</p>
-                <button onClick={() => buy(prop)}>Buy</button>
-            </div>
-        )
-    }
-
+  function NftCard(prop) {
     return (
-        <div className={styles.store}>
-            <p>Store</p>
+      <div className="relative flex  w-1/3 flex-col rounded-xl bg-gel-black text-sm overflow-hidden">
+        <div className="flex flex-1 flex-col">
+          <div className="rounded-t-3xl ">
+            <img className="w-full" src={prop.image} alt="" width="200px" />
+          </div>
+          <div className="p-6">
+            {/* <div className="mt-2 flex-1 text-2xl font-bold transition-all delay-75 duration-200 group-hover:-translate-y-5 group-hover:scale-105 group-hover:opacity-0">
+                Harvesting Yield Farming Vaults
+            </div> */}
             <div>
-                {nfts.map((item, i) => (
-                    <NftCard
-                        key={i}
-                        price={item.price}
-                        name={item.name}
-                        tokenId={item.tokenId}
-                        supply={item.supply}
-                        image={item.image}
-                        remaining={item.remaining}
-                    />
-                ))}
+            <p>{prop.tokenId}</p>
             </div>
+            <div className="flex justify-between">
+              <p>{prop.name}</p>
+              <p>{prop.price}</p>
+            </div>
+            <div className="flex justify-between">
+              <p>{prop.supply}</p>
+              <p>{prop.remaining}</p>
+            </div>
+            <div className="flex justify-between">
+                
+              <button className="font-medium px-2 text-xs lg:text-base lg:px-5 py-2 text-white rounded-xl lg:rounded-2xl hover:opacity-70 bg-blue-600" onClick={() => buy(prop)}>Buy</button>
+              <p>{prop.price}</p>
+            </div>
+          </div>
+
+          <div className="use-case-projects flex transition-all delay-100 duration-200 group-hover:translate-y-5 group-hover:scale-105 group-hover:opacity-0"></div>
         </div>
-    )
+      </div>
+
+      // <div>
+      //     <p>Card</p>
+      //     <img src={prop.image} alt="" width="200px"/>
+      //   <p>{prop.price}</p>
+      //   <p>{prop.name}</p>
+      //   <p>{prop.tokenId}</p>
+      //   <p>{prop.supply}</p>
+      //   <p>{prop.remaining}</p>
+      //   <button onClick={() => buy(prop)}>Buy</button>
+      // </div>
+    );
+  }
+
+  return (
+    <div className="pt-40">
+      <p>Store</p>
+      <div>
+        {nfts.map((item, i) => (
+          <NftCard
+            key={i}
+            price={item.price}
+            name={item.name}
+            tokenId={item.tokenId}
+            supply={item.supply}
+            image={item.image}
+            remaining={item.remaining}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
