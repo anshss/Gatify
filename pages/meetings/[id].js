623659dec69d87380cc21b1bb12c8aca3cc8d148
Sarthak Vaish <sarthak@simplifii.com>
2023-05-03 18:19:07 +0530

    meeting page fix

--- pages/meetings/[id].js
+++ pages/meetings/[id].js
@@ -2,8 +2,11 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react';
 import { HuddleIframe, IframeConfig } from "@huddle01/huddle01-iframe";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default  function Profile() {
+
 
     const [ meetLink, setMeetLink ] = useState()
     
@@ -23,14 +26,18 @@ export default  function Profile() {
 
     const iframeConfig = {
         roomUrl: `https://iframe.huddle01.com/${id}`,
-        height: "600px",
+        height: "700px",
         width: "100%",
         noBorder: false, // false by default
       };
     
     return (
-        <div className="pt-40">
-            <button className="flex text-md mb-5" onClick={() => { navigator.clipboard.writeText(meetLink) }}>{meetLink} &nbsp; <img src="../copy.svg" /></button>
+        <div className="pt-48 min-h-screen pb-20">
+            <div className='flex gap-2'>
+                <p>Meeting Link: </p>
+                <button className="flex text-md mb-5" onClick={() => { navigator.clipboard.writeText(meetLink); toast("Text copied successfully") }}>{meetLink} &nbsp; <img className="hover:opacity-60" src="../copy.svg" /></button>
+            </div>
+            <ToastContainer />
             
             <HuddleIframe config={iframeConfig} />
         </div>
